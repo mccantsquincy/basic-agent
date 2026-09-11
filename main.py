45df66f8ad2f39421b3f10f75f25657cea36c579
mccantsquincy <quincymccants@gmail.com>
2026-09-10 22:44:42 -0700

    Add first OpenAI call

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Basic agent is running")
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+response = client.responses.create(
+    model="gpt-4o-mini",
+    input="Say hello and tell me you are running in my phython project",
+)
+
+print(response.output_text)
